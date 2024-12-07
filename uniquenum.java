a02128a53706f141d5e6040d39a2b57c5cf028c2
assu786 <shaikaqsa.78@gmail.com>
2024-12-07 14:24:19 +0530

    Add files via upload

---
+++ uniquenum.java
@@ -0,0 +1,21 @@
+import java.util.*;
+class S
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int arr[]=new int[n];
+        int ans=0;
+        for(int i=0;i<n;i++)
+        {
+            arr[i]=sc.nextInt();
+        }
+        for(int i=0;i<n;i++)
+        {
+            ans^=arr[i];
+        }
+        System.out.println(ans);
+    }
+
+}
